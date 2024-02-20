c441fa1d3a74ea0eeaf559bcd8211896f17af828
Alice Escalera <alice.escalera@gmail.com>
2024-02-20 18:05:20 +0100

    write test to check if a discount has already been applied on the product

--- src/app.js
+++ src/app.js
@@ -1,21 +1,124 @@
-class Cube {
-    constructor(length) {
-        this.length = length;
+
+class Cart {
+  articles = [];
+  price = [];
+
+  constructor() {}
+
+  addArticle(article, prix) {
+    this.articles.push({ article, prix });
+    this.price.push(prix);
+    return this.articles;
+  }
+
+  getTotalPrice() {
+    return this.articles
+      .map((val) => val.prix)
+      .reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      }, 0);
+  }
+
+  deleteArticles(article) {
+    let ind = this.articles.findIndex(art => art.article === article);
+    if(ind !== -1) {
+      this.articles.splice(
+          ind, 1
+      );
+      return "votre article :" + article + "a été supprimé";
+    } else {
+      return "Votre article n'a pas été supprimé !"
     }
-    
-    getSideLength () {
-        return this.length;
+  }
+
+  emptyCart() {
+    this.articles.splice(0, this.articles.length);
+    return "votre panier est vide";
+  }
+}
+
+class Discount {
+  articles = []
+  constructor() {
+  }
+
+  addDiscount(article) {
+  }
+
+  getDiscount(article) {
+    return article.discount
+  }
+
+  applyDiscount(article) {
+    if(this.hasDiscount(article)) {
+      if(article.discount >article.price) {
+        throw new Error('la remise doit-être inférieure ou égale au prix du produit');
+      }
+      return article.price - (article.discount);
     }
-    
-    getSurfaceArea () {
-        return (this.length * this.length) * 6;
+  }
+
+  hasDiscount(article) {
+    if(article.discount) {
+      return true
+    } else {
+      return false
     }
-    
-    getVolume () {
-        return Math.pow(this.length,3);
+  }
+
+}
+class Product {
+  price =0;
+  name="";
+  datePeremption;
+  creation_date;
+  deleted_date="";
+  constructor(price, name, datePeremption, creationDate=Date.now) {
+    this.price = price;
+    this.name = name;
+    this.datePeremption = datePeremption;
+    this.creation_date=creationDate;
+  }
+
+
+
+}
+class Stock {
+  productList = [] ;
+  productListHistory;
+  constructor(list) {
+    this.productList=list;
+  };
+  addProduct(name, price, datePeremption) {
+    let p = new Product(name=name, price=price, datePeremption=datePeremption);
+    this.productList.push(p)
+  }
+
+  deleteProduct(product) {
+    if (productList && productList.length > 0) {
+      let ind = this.productList.findIndex(art => art.article === article);
+      if (ind !== -1) {
+        this.productList[ind].deleted_date = Date.now();
+      } else {
+        return "article introuvable"
+      }
     }
+  }
+  getStock() {
+    productList.forEach(el => console.log(el));
+  }
+
+  compareStock(first_date, second_date) {
+    let stockBis = productList.map(el => el.creation_date<first_date);
+    console.log("à date du : "+first_date+" il y a "+ stockBis.length+ " produits")
+    console.log("à date du : "+second_date+" il y a "+ productList.length+" produits")
+  }
+
 }
 
 module.exports = {
-    Cube:Cube
-}
+  Cart: Cart,
+  Discount: Discount,
+  Stock: Stock,
+  Product: Product,
+};
--- test/app.test.js
+++ test/app.test.js
@@ -1,23 +1,59 @@
-const Cube = require('../src/app').Cube;
-const expect = require('chai').expect;
+const Discount = require("../src/app").Discount;
+const Product = require("../src/app").Product;
+const Stock = require("../src/app").Stock;
+const expect = require("chai").expect;
 
-describe('Testing the Cube Functions', function() {
-    it('1. The side length of the Cube', function(done) {
-        let c1 = new Cube(2);
-        expect(c1.getSideLength()).to.equal(2);
-        done();
-    });
-    
-    it('2. The surface area of the Cube', function(done) {
-        let c2 = new Cube(5);
-        expect(c2.getSurfaceArea()).to.equal(150);
-        done();
-    });
-    
-    it('3. The volume of the Cube', function(done) {
-        let c3 = new Cube(7);
-        expect(c3.getVolume()).to.equal(343);
-        done();
-    });
-    
+describe("Testing the Cart Functions", function () {
+  it("1. Add product in cart", function (done) {
+    let c = new Cart();
+    expect(c.addArticle("chaussette", 40)).to.deep.equal([
+      { article: "chaussette", prix: 40 },
+    ]);
+    done();
+  });
+  it("2. Get the total price of the cart", function() {
+    let c = new Cart();
+    c.articles = [{ article:"chaussette", prix: 10 }, { article:"short", prix: 20 }, { article:"pull", prix: 30 }];
+    const expectedTotalPrice = 60;
+    expect(c.getTotalPrice()).to.equal(expectedTotalPrice);
+  })
+  it("3. delete an article", function() {
+    let c = new Cart();
+    c.articles = [{ article:"chaussette", prix: 10 }, { article:"short", prix: 20 }, { article:"pull", prix: 30 }];
+    res = [{ article:"short", prix: 20 }, { article:"pull", prix: 30 }]
+    c.deleteArticles("chaussette");
+    expect(c.articles).to.deep.equal([{ article:"short", prix: 20 }, { article:"pull", prix: 30 }]);
+  })
+  it("4. empty cart",function() {
+    let c = new Cart()
+    c.articles = [{ article:"chaussette", prix: 10 }, { article:"short", prix: 20 }, { article:"pull", prix: 30 }];
+    res = [{ article:"short", prix: 20 }, { article:"pull", prix: 30 }]
+    c.emptyCart();
+    expect(c.articles.length).to.equal(0);
+  })
 });
+
+
+describe("Testing the Discount Functions", function () {
+
+  it("0. vérifier que le coupon a bien été appliqué", function() {
+    let d = new Discount();
+    article = {article: "chaussette", price: 40, discount: 10};
+    expect(d.applyDiscount(article)).to.equal(30);
+  })
+  it("1. vérifier que le coupon existe", function() {
+    let d = new Discount();
+    article = {article: "chaussette", price: 40, discount: ""};
+    expect(d.hasDiscount(article)).false;
+  })
+  it("2.le prix de l'article ne peut pas être négatif", function() {
+    let d = new Discount();
+    article = {article: "chaussette", price: 40, discount: 150};
+    expect(d.applyDiscount(article)).to.throw();
+  })
+  it("3. un coupon ne peut être utilisé qu'une seule fois sur un article", function() {
+    let d = new Discount();
+    article = {article:"chaussette", price:40, discount:150, applied:false};
+    expect(d.couponApplied(coupon, article)).to.equal({article:"chaussette", price:40, discount:150, applied:true})
+  })
+})
